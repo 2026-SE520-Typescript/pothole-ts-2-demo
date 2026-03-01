a414e985dc665bbc1f1f130555a95e0b3903bf2d
Denys Zavhorodnii <denis.zavgorodny@gmail.com>
2026-03-01 17:16:16 +0200

    fix types

--- src/stories/Tabs.stories.tsx
+++ src/stories/Tabs.stories.tsx
@@ -3,34 +3,32 @@ import type {
     StoryObj
 } from '@storybook/react-webpack5';
 
-import {Header} from '../components/header/Header';
 import {Tabs} from '../components/tabs/Tabs';
 import React from 'react';
 
-
-const meta: Meta<typeof Header> = {
+const meta: Meta<typeof Tabs> = {
     component: Tabs,
     tags: ['autodocs'],
     title: 'Tabs',
     args: {
         tabs: [
             {
-                id: 1,
+                id: '1',
                 label: 'Tab1',
                 content: <div>Hello11111</div>
             },
             {
-                id: 2,
+                id: '2',
                 label: 'Tab2',
                 content: <div>Hello22222</div>
             },
             {
-                id: 3,
+                id: '3',
                 label: 'Tab3',
                 content: <div>Hello33333</div>
             }
         ]
-    }
+    } satisfies Partial<React.ComponentProps<typeof Tabs>>
 };
 
 export default meta;
