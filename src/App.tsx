7ca32a234590455269785ddcede47898250ac66f
Denys Zavhorodnii <denis.zavgorodny@gmail.com>
2026-03-12 10:26:41 +0200

    add basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,34 @@ import React from 'react';
 import styles from './styles.css';
 import {Header} from './components/header/Header';
 import {Counter} from './components/counter/Counter';
+import {Logo} from './components/logo/Logo';
+import {Login} from './components/login/Login';
+import {Content} from './components/content/Content';
+import {Tabs} from './components/tabs/Tabs';
+import {Tab} from './components/tabs/Tab';
 
 export const App: React.FC = () => {
+    const tabItems = [
+        {
+            id: '1',
+            label: 'Map',
+            content: <div>Hello11111</div>
+        },
+        {
+            id: '2',
+            label: 'List',
+            content: <div>Hello22222</div>
+        }
+    ];
     return <div className={styles.layout}>
         <Header>
-            <Counter counter={10} label='Active' />
+            <Logo />
+            <Counter counter={10} label='Active' active={false} />
+            <Counter counter={10} label='Closed' active={false} />
+            <Login isLogged={false} />
         </Header>
+        <Content>
+            <Tabs tabs={tabItems} renderFunction={(label, tabId, activeTab) => <Tab title={label} active={activeTab === tabId} />} />
+        </Content>
     </div>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM, {Container} from 'react-dom/client';
 import {App} from './App';
+import Type from 'typebox';
+import Compile from 'typebox/compile';
+import Value from 'typebox/value';
 
 
 const root = ReactDOM.createRoot(
@@ -8,3 +11,22 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(<App />);
+
+const schema = Type.Object({
+    a: Type.Number()
+});
+
+type T = Type.Static<typeof schema>
+
+
+const a = Compile(schema);
+try {
+    const errors = Value.Errors(schema, {a: '1'});
+    console.log(errors);
+    console.log(a.Parse({a: 'some value'}));
+    console.log(a.Check({a: 'some value'}));
+} catch (err) {
+    console.log(err);
+}
+
+
