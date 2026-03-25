48f232d8f711ad9332154d49088f0f308fafe202
Denys Zavhorodnii <denis.zavgorodny@gmail.com>
2026-03-25 22:13:19 +0200

    add styles

--- src/pages/guard.tsx
+++ src/pages/guard.tsx
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import {UserContext} from '../context/userContext';
 import {Link} from 'react-router';
+import styles from './guard.css';
 
 type GuardType = {
     children: React.ReactNode;
@@ -9,5 +10,17 @@ type GuardType = {
 export const Guard: React.FC<GuardType> = ({children}) => {
     const userContext = useContext(UserContext);
 
-    return userContext._type === 'anonymous' ? <div>Forbidden <Link to={'/'}>Main page</Link></div> : children;
+    if (userContext._type === 'anonymous') {
+        return (
+            <div className={styles.container}>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>Forbidden</h1>
+                    <p className={styles.message}>You need to be logged in to view this page.</p>
+                    <Link className={styles.link} to={'/'}>Main page</Link>
+                </div>
+            </div>
+        );
+    }
+
+    return children;
 };
