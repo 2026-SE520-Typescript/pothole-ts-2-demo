47f340ec9ed41ed305ecc8831c5931604a9ea464
Denys Zavhorodnii <denis.zavgorodny@gmail.com>
2026-04-01 23:50:27 +0300

    use base url

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,7 @@ module.exports = {
             template: './public/index.html'
         }),
         new webpack.DefinePlugin({
+            'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
             'process.env.MAP_KEY': JSON.stringify(process.env.MAP_KEY ?? ''),
             'process.env.API_KEY': JSON.stringify(process.env.API_KEY ?? ''),
             'process.env.MY_PASSWORD': JSON.stringify(process.env.MY_PASSWORD ?? '')
